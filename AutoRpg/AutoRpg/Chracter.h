163a72e8da7f8bc446893b90631fe4bf84f77ad8
YOUNG <whdudghks8@naver.com>
2025-01-10 14:17:07 +0900

    Charcter.h 추가했습니다
    간단하게 생각나는대로 구현

---
+++ AutoRpg/AutoRpg/Chracter.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class Character {
+public:
+    string name;
+    int level;
+    int maxHp;
+    int hp;
+    int attack;
+    int experience;
+    int gold;
+};
