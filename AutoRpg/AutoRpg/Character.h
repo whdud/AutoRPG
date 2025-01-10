dbe67552cf5148fdf33941dabef39221b009c3c8
YOUNG <whdudghks8@naver.com>
2025-01-10 20:32:26 +0900

    getter and setter

--- AutoRpg/AutoRpg/Character.h
+++ AutoRpg/AutoRpg/Character.h
@@ -7,6 +7,33 @@ class Character {
 public:
     Character();
 	void LevelUp();
+    // 이름 게터와 세터  
+    string getName() const { return mName; }
+    void setName(const string& name) { mName = name; }
+
+    // 레벨 게터와 세터
+    int getLevel() const { return mLevel; }
+    void setLevel(int level) { mLevel = level; }
+
+    // 최대최력 게터와 세터
+    int getMaxHp() const { return mMaxHp; }
+    void setMaxHp(int maxHp) { mMaxHp = maxHp; }
+
+    // 체력 게터와 세터
+    int getHp() const { return mHp; }
+    void setHp(int hp) { mHp = hp; }
+
+    // 공격력 게터와 세터
+    int getAttack() const { return mAttack; }
+    void setAttack(int attack) { mAttack = attack; }
+
+    // 경험치 게터와 세터
+    int getExperience() const { return mExperience; }
+    void setExperience(int experience) { mExperience = experience; }
+
+    // 골드 게터와 세터
+    int getGold() const { return mGold; }
+    void setGold(int gold) { mGold = gold; }
 protected:
     void displayStatus();
 	void resetCharacter();
