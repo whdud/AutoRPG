5c20b2ea1918cd4742f2ce7b8f1ad7a70d959c08
yebinr7 <yebinr7@gmail.com>
2025-01-13 14:18:45 +0900

    define에 쉐어드포인터 추가
    ㅇㅇ

--- AutoRpg/AutoRpg/defines.h
+++ AutoRpg/AutoRpg/defines.h
@@ -4,6 +4,19 @@
 //한글 안깨지나?
 #define GET_SINGLE(classname) classname::GetInstance()
 
+// TSharedPtr 템플릿 정의
+template <typename T>
+using TSharedPtr = std::shared_ptr<T>;
+
+// TSharedRef 템플릿 정의 (TSharedPtr와 동일하게 동작)
+template <typename T>
+using TSharedRef = TSharedPtr<T>;
+
+// TWeakPtr 템플릿 정의
+template <typename T>
+using TWeakPtr = std::weak_ptr<T>;
+
+
 #define SAFE_DELETE(ptr)  			  \
 	if (ptr)						  \
 	{								  \
