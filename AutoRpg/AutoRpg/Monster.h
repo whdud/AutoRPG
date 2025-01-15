3a8c69d7930fc00707370e26bc52b531f86c8a08
YOUNG <whdudghks8@naver.com>
2025-01-15 10:17:00 +0900

    Monster

--- AutoRpg/AutoRpg/Monster.h
+++ AutoRpg/AutoRpg/Monster.h
@@ -1,6 +1,21 @@
 ﻿#pragma once
+#include <string>
+using namespace std;
+
 class Monster {
+protected:
+    string mName;    // 몬스터 이름
+    int mHealth;     // 몬스터 체력
+    int mAttack;     // 몬스터 공격력
+    string mAsciiArt; // ASCII 아트 문자열
+
 public:
-	Monster();
-	~Monster();
+    Monster(int level, const string& type, const string& asciiArt);
+
+    string GetName() const;
+    int GetHealth() const;
+    int GetAttack() const;
+    void TakeDamage(int damage);
+    string GetAsciiArt() const; // ASCII 아트 반환
+    virtual ~Monster() {}
 };
