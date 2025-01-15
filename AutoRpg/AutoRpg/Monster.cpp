c6f452b1b502f116151dbf754ab9ca24bb93e5a7
shilloon <146956901+shilloon@users.noreply.github.com>
2025-01-15 16:13:43 +0900

    itemdrop

--- AutoRpg/AutoRpg/Monster.cpp
+++ AutoRpg/AutoRpg/Monster.cpp
@@ -1,5 +1,6 @@
 ﻿#include "Monster.h"
 #include <iostream>
+
 using namespace std;
 
 Monster::Monster(int level)
@@ -8,10 +9,44 @@ Monster::Monster(int level)
     mAttack = rand() % (level * 5) + (level * 5);   // 공격력 설정
 }
 
-
-
 string Monster::GetName() const { return mName; }
 int Monster::GetMonsterHp() const { return mMonsterHp; }
 int Monster::GetAttack() const { return mAttack; }
 void Monster::TakeDamage(int damage) { mMonsterHp -= damage; }
 
+void Monster::DropItem(Character& character)
+{
+
+    // if monster is not died return
+
+    //  item drop 30%
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<>dist(1, 100);
+    int DropPercent = dist(gen);
+
+    if (DropPercent <= 30)
+    {
+
+        uniform_int_distribution<> ItemDist(1, 2);
+        int ItemType = ItemDist(gen);
+
+        shared_ptr<Item> DroppedItem;
+        if (ItemType == 1)
+        {
+
+            DroppedItem = make_shared<HealthPotion>(10, 50);
+
+        }
+        else if (ItemType == 2)
+        {
+
+            DroppedItem = make_shared<AttackBoost>(20, 10);
+
+        }
+
+        character.AddItem(DroppedItem);
+        cout << "Monster dropped Item : " << DroppedItem->GetName() << endl;
+
+    }
+}
--- AutoRpg/AutoRpg/Monster.h
+++ AutoRpg/AutoRpg/Monster.h
@@ -1,13 +1,21 @@
 ﻿#pragma once
 #include <string>
+#include <memory>
+#include <random>
+#include "Character.h"
+#include "Item.h"
+#include "HealthPotion.h"
+#include "AttackBoost.h"
+#include "Inventory.h"
+
 using namespace std;
 
 class Monster {
+
 protected:
     string mName;    // 몬스터 이름
     int mMonsterHp;     // 몬스터 체력
     int mAttack;     // 몬스터 공격력
-   
 
 public:
     Monster(int level);
@@ -18,4 +26,7 @@ public:
     void TakeDamage(int damage);
     virtual string GetAsciiArt() const =0; // ASCII 아트 반환
     virtual ~Monster() {}
+
+    void DropItem(Character& character); // Drop Item
+
 };
